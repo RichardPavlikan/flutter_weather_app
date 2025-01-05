d5eef4315eae4b5c73e5b59ad7d927c5d000d394
Richard Pavlikán <richardpavlikan@gmail.com>
2025-01-05 02:10:58 +0100

    project

---
+++ lib/constants/weather_enum.dart
@@ -0,0 +1,10 @@
+/// [WeatherEnum] is an enum class that contains all the weather conditions.
+enum WeatherEnum {
+  thunderstorm,
+  drizzle,
+  rain,
+  snow,
+  atmosphere,
+  clear,
+  clouds,
+}
---
+++ lib/presentation/widgets/loading_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+/// [LoadingWidget] is the loading widget for the application.
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(
+        color: Colors.white,
+      ),
+    );
+  }
+}
